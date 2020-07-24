7ad0c6e537b39fada8fe53404bf98eff63912043
swfarnsworth <swfarnsworth@gmail.com>
2020-07-24 18:36:38 -0400

    created setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='bratlib',
+    packages=find_packages(),
+    version='0.0.0',
+    description='Data facilitation for BRAT annotations',
+    author='Steele Farnsworth',
+    install_requires=[
+        'cached-property',
+    ]
+)
