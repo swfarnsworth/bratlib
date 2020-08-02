e4ef2a29a0a0bb36edc54cad05b9e01ec0218a84
swfarnsworth <swfarnsworth@gmail.com>
2020-08-02 13:11:26 -0400

    never called zip

--- bratlib/iteration.py
+++ bratlib/iteration.py
@@ -7,5 +7,5 @@ from bratlib.data import BratDataset
 
 def zip_datasets(*datasets: BratDataset) -> t.Iterable[t.Tuple[BratDataset]]:
     matching_anns = reduce(and_, ({a.name for a in ds} for ds in datasets))
-    iterators = (filter(lambda a: a.name in matching_anns, sorted(ds, key=attrgetter('name'))) for ds in datasets)
-    yield from iterators
+    iterators = [filter(lambda a: a.name in matching_anns, sorted(ds, key=attrgetter('name'))) for ds in datasets]
+    yield from zip(*iterators)
