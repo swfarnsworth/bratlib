f9076a2651ecfc0226e668e2582fb91329288ed9
swfarnsworth <swfarnsworth@gmail.com>
2020-08-11 00:05:47 -0400

    added get_labels

--- bratlib/data/extensions/file.py
+++ bratlib/data/extensions/file.py
@@ -1,9 +1,15 @@
+import typing as t
 from collections import Counter
+from functools import reduce
+from operator import or_
 
 from bratlib.data import BratDataset
 
 
 class StatsDataset(BratDataset):
+
+    def get_labels(self) -> t.Set[str]:
+        return reduce(or_, ({e.mention for e in a.entities} for a in self))
 
     def count_entities(self):
         return sum((Counter(ent.tag for ent in f.entities) for f in self), Counter())
