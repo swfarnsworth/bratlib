1d72ff17d65a5f85af6696f466e3c1cf20b3ba11
Steele <swfarnsworth@gmail.com>
2021-01-19 13:11:37 -0500

    Use two validation functions, return a DataFrame of Entity -> bool.

--- bratlib/tools/validation.py
+++ bratlib/tools/validation.py
@@ -1,29 +1,39 @@
 import argparse
 import os
-import typing as t
-from functools import reduce
-from operator import add
+import re
+
+import pandas as pd
 
 from bratlib.data import BratDataset, BratFile, Entity
 
 
-def validate_entity(ent: Entity, text: str) -> bool:
-    mention_in_text = reduce(add, (text[a:b] for a, b in ent.spans))
-    valid = ent.mention == mention_in_text
-    if not valid:
-        print(ent, mention_in_text)
-    return valid
+def _validate_contiguous_entity(ent: Entity, text: str) -> bool:
+    a, b = ent.spans[0]
+    return ent.mention == text[a:b]
 
 
-def validate_bratfile(ann: BratFile) -> t.List[bool]:
-    with ann.txt_path.open() as f:
-        text = f.read()
-    return [validate_entity(ent, text) for ent in ann.entities]
+def _validate_noncontiguous_entity(ent: Entity, text: str) -> bool:
+    mention = r'\s*'.join(re.escape(text[a:b]) for a, b in ent.spans)
+    return bool(re.fullmatch(mention, ent.mention))
 
 
-def validate_bratdataset(data: BratDataset) -> None:
+def validate_bratfile(ann: BratFile) -> pd.DataFrame:
+    text = ann.txt_path.read_text()
+    return pd.DataFrame.from_dict(
+        {e: (_validate_contiguous_entity if len(e.spans) == 1 else _validate_noncontiguous_entity)(e, text)
+         for e in ann.entities},
+        orient='index', columns=['match']
+    )
+
+
+def validate_bratdataset(data: BratDataset) -> pd.DataFrame:
+    outer_df = pd.DataFrame()
     for ann in data:
-        validate_bratfile(ann)
+        df = validate_bratfile(ann)
+        df['file_name'] = ann.ann_path.stem
+        df.set_index('file_name', append=True)
+        outer_df = outer_df.append(df)
+    return outer_df
 
 
 def main():
