da44191d041c6908539dd03de763974f14d49960
Steele <swfarnsworth@gmail.com>
2021-01-21 12:11:21 -0500

    Bump bugfix version number, since this is a pre-release version.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 setup(
     name='bratlib',
     packages=find_packages(),
-    version='0.0.0',
+    version='0.0.1',
     description='Data facilitation for BRAT annotations',
     author='Steele Farnsworth',
     install_requires=[
