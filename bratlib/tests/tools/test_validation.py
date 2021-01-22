930291c10e24554681a34b47a938388ca54173bd
Steele <swfarnsworth@gmail.com>
2021-01-22 10:30:31 -0500

    Added dataset fixture for future tests.

--- bratlib/tests/tools/test_validation.py
+++ bratlib/tests/tools/test_validation.py
@@ -1,26 +1,34 @@
+import pytest
 import pandas as pd
 
 from bratlib import data as bd
 from bratlib.tools.validation import validate_bratfile_entities
 
 
-def test_validate_bratfile(tmp_path):
-    text_path = tmp_path / 'a.txt'
-    text_path.write_text("The quick brown fox jumped over the lazy dog.")
-    ann_path = tmp_path / 'a.ann'
+@pytest.fixture
+def brat_dataset(tmp_path):
+    dataset_path = tmp_path / 'data'
+    dataset_path.mkdir()
 
-    entities = [
+    a_file = bd.BratFile.from_data(entities=[
         bd.Entity('A', [(4, 9)], 'quick'),
         bd.Entity('A', [(4, 9), (20, 26)], 'quick jumped'),
         bd.Entity('A', [(4, 9)], 'not quick'),
         bd.Entity('A', [(4, 9), (20, 26)], 'not quick jumped'),
-    ]
+    ])
 
-    ann = bd.BratFile.from_data(entities=entities)
-    ann.ann_path, ann._txt_path = ann_path, text_path
+    a_ann, a_txt = (dataset_path / 'a.ann'), (dataset_path / 'a.txt')
+    a_txt.write_text("The quick brown fox jumped over the lazy dog.")
+    a_ann.write_text(str(a_file))
+    a_file.ann_path, a_file._txt_path = a_ann, a_txt
 
+    return bd.BratDataset(dataset_path, [a_file])
+
+
+def test_validate_bratfile_entities(brat_dataset):
+    ann = brat_dataset.brat_files[0]
     expected = pd.DataFrame.from_dict(
-        {e: b for e, b in zip(entities, [True, True, False, False])},
+        dict(zip(ann.entities, [True, True, False, False])),
         orient='index', columns=['match']
     )
     actual = validate_bratfile_entities(ann)
